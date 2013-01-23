8f0e111a5fcc41ee530a09e14b3f12d0082926a7
Steve Testen <testen@gmail.com>
2013-01-22 21:13:12 -0500

    Started Problem 13
    Set up problem 13

---
+++ Problems/problem013/problem013.java
@@ -0,0 +1,22 @@
+/* Work out the first ten digits of the sum of
+the following one-hundred 50-digit numbers. 
+(Found in problem013.txt) */
+
+//use BigInteger? (for cheaters)
+
+import java.io.*;
+import java.util.*;
+
+public class problem013
+{
+	public static void main(string[] args) throws FileNotFoundException
+	{
+		File txtfile = new File("problem013.txt");
+		Scanner numfile = new Scanner(txtfile);
+		int [] [] num_array = new [100] [50];
+
+		int row = 0;
+	}
+
+
+}
