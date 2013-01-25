59063647f2d64f9e27dd9e20c7d19dfd09c58a9a
Steve Testen <testen@gmail.com>
2013-01-24 22:10:28 -0500

    Problem 13 Complete
    Completed problem 13 using BigInteger...

--- Problems/problem013/problem013.java
+++ Problems/problem013/problem013.java
@@ -6,16 +6,27 @@ the following one-hundred 50-digit numbers.
 
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class problem013
 {
-	public static void main(string[] args) throws FileNotFoundException
+	public static void main(String[] args) throws FileNotFoundException
 	{
 		File txtfile = new File("problem013.txt");
 		Scanner numfile = new Scanner(txtfile);
-		int [] [] num_array = new [100] [50];
+		BigInteger sum = new BigInteger("0");
 
-		int row = 0;
+		while (numfile.hasNextLine())
+		{
+			BigInteger nextline = new BigInteger(numfile.next());
+			sum = sum.add(nextline);
+
+		}
+		
+		System.out.println("The sum is: " + sum);
+		String answer = (sum.toString()).substring(0,10);
+		System.out.println("The answer is: " + answer);
+
 	}
 
 
